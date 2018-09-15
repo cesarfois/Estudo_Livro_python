87e1bb4a22af4f988c1883deb29c8db288a52e5c
César J. Fois <fois2010@gmail.com>
2018-09-15 09:22:31 -0300

    Adicionado Listagem 3.7 A função len v4 TDD

--- #Livro_Introduçao a programaçao python/Listagem 3.7 A função len v3 TDD.py
+++ #Livro_Introduçao a programaçao python/Listagem 3.7 A função len v3 TDD.py
@@ -1,7 +1,7 @@
 import unittest
 
 print('='*72)
-print('{0:=^72}'.format(' Listagem 3,7 v4 '))
+print('{0:=^72}'.format(' Listagem 3,7 v3 '))
 print('{0:=^72}'.format(' By César J. Fois '))
 print('='*72)
 print('A função len')
@@ -18,7 +18,7 @@ class Len_String():
 
 class len_String_Test(unittest.TestCase):
 
-    def test_len_string_one_character(self):
+    def test_len_string(self):
         self.assertListEqual([len('a'), len('AB'), len(''), len('o rato roe a roupa')],
                              [1, 2, 0, 18])
 
