bfaebf64cabc24b97cab6cc9e5cf3c4eda30da37
César J. Fois <fois2010@gmail.com>
2018-12-18 15:39:15 -0200

    uri 1041

--- #Livro_Introd-Prog-Python/1041.py
+++ #Livro_Introd-Prog-Python/1041.py
@@ -1,22 +1,24 @@
-
 lista = input().split()
 
 x = float(lista[0])
 y = float(lista[1])
 
 
+if (x == 0.0) and (y == 0.0):
+    print("Origem")
+
+if(x == 0.0) and (y != 0.0):
+    print("Eixo Y")
+if(x != 0.0) and (y == 0.0):
+    print("Eixo X")
+
 if (x > 0) and (y > 0):
     print("Q1")
-elif(x == 0) and (y != 0):
-    print("Eixo X")
-elif(x != 0) and (y == 0):
-    print("Eixo Y")
-elif (x > 0) and (y < 0):
-    print("Q4")
-elif (x < 0) and (y > 0):
+if (x < 0) and (y > 0):
     print("Q2")
-elif (x < 0) and (y < 0):
+if (x < 0) and (y < 0):
     print("Q3")
-elif (x == 0) and (y == 0):
-    print("Origem")
+if (x > 0) and (y < 0):
+    print("Q4")
+
 
