2bad48dd4962b7d83869229f95410b6ee8ba4699
César J. Fois <fois2010@gmail.com>
2018-12-20 15:53:22 -0200

    uri 1061

---
+++ #Livro_Introd-Prog-Python/1061.py
@@ -0,0 +1,15 @@
+diai = input().split()
+tempoi = input().split(' : ')
+diaf = input().split()
+tempof = input().split(' : ')
+
+di = int(diai[1])
+df = int(diaf[1])
+hi, mi, si = tempoi
+hf, mf, sf = tempof
+
+
+print('%d dia(s)' %(df - di))
+print('%d hora(s)' %hf)
+print('%d hora(s)' %hf)
+print('%d segundo(s)' %hf)
