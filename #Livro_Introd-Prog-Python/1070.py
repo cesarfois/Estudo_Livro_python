ba4ea7a67dddc8e2435ca0c74c401a287069e978
César J. Fois <fois2010@gmail.com>
2018-12-22 10:13:15 -0200

    uri 1078

--- #Livro_Introd-Prog-Python/1070.py
+++ #Livro_Introd-Prog-Python/1070.py
@@ -1,5 +1,14 @@
-x = int(input())
+a = int(input())
+b = int(input())
+res = 0
 
-for x in range(x, (x+13), 1):
-    if x % 2 != 0:
-        print(x)
+if a < b:
+    for x in range((a+1), b, 1):
+        if x % 2 != 0:
+            res = res + x
+if a > b:
+    for x in range((b+1), a, 1):
+        if x % 2 != 0:
+            res = res + x
+
+print(res)
