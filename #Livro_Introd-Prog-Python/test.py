931fe8bb472188f7ccac67b7f5c69c61577aba50
César J. Fois <fois2010@gmail.com>
2018-12-28 23:38:13 -0200

    uri 1187

--- #Livro_Introd-Prog-Python/test.py
+++ #Livro_Introd-Prog-Python/test.py
@@ -1,4 +1,14 @@
-valor = float(input())
-for i in range(0,100):
-    print("N[%d] = %.4f" %(i,valor))
-    valor /= 2.0
+soma = 0
+lin = 12
+lin2 = 0
+
+for c in range(12):
+    lin = lin - 1
+    lin2 = lin2 + 1
+    for j in range(12):
+        if (j < lin) and (j >= lin2):
+            print('%4d' % j, end='|')
+            soma = soma + 1
+    print()
+
+print(soma)
